7b1e406a65cca9459e03a015492fb7211066d76d
“rpr_maddi” <ram.prasad@introhive.com>
2017-11-20 21:59:42 +0530

    Thoughtlog: Base commit.
    * Included webpack for compiling, prefixing and making the build
      with all the depedencies loaded.

---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const AutoPrefixer = require('autoprefixer');
+const glob = require('glob-all');
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+}
+
+// const commonConfig = {
+//   entry: {
+//     app: PATHS.app,
+//   },
+//   output: {
+//     path: PATHS.build,
+//     filename: "[name].js",
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'index.html'
+//     }),
+//     new webpack.WatchIgnorePlugin([
+//       path.join(__dirname, "node_modules")
+//     ]),
+//   ],
+// };
+
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: "[name].js",
+    },
+    resolve: {
+      modules: [
+        "node_modules",
+        PATHS.app
+      ],
+      extensions: [".js", ".jsx", ".scss"]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, "node_modules")
+      ]),
+      new ExtractTextPlugin({
+        allChunks: true,
+        filename: "[name].css",
+      }),
+      new PurifyCSSPlugin({
+        paths: glob.sync(path.join(__dirname, '*.html'))
+      }),
+    ],
+  },
+  parts.loadSCSS({ exclude: /node_modules/ }),
+]);
+
+const prodConfig = merge([]);
+
+// const prodConfig = () => {
+//   return commonConfig;
+// };
+
+// const devConfig = () => {
+//   const config = {
+//     devServer: {
+//       contentBase: PATHS.build,
+//       host: 'localhost' || process.env.host,
+//       port: '8080' || process.env.PORT,
+//       overlay: {
+//         errors: true,
+//         warnings: true,
+//       },
+//       watchOptions: {
+//         aggregateTimeout: 300,
+//         poll: 1000,
+//       },
+//     }
+//   };
+//   return Object.assign({}, commonConfig, config);
+// }
+  
+const devConfig = merge([
+  parts.devServer({
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || '8080'
+  })
+]);
+
+
+// module.exports = (env) => {
+// 
+//   if (env === "production") {
+//     return prodConfig();
+//   }
+// 
+//   return devConfig();
+// };
+
+module.exports = (env) => {
+  if (env === "production") {
+    return merge(commonConfig, prodConfig);
+  }
+  return merge(commonConfig, devConfig);
+};
+
+
+
+
---
+++ webpack.parts.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+exports.devServer = ({ host = 'loalhost', port = '8080'} = {}) => ({
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    stats: "errors-only",
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
+});
+
+exports.loadSCSS = ({include, exclude} = {}) => {
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          include, 
+          exclude,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              { loader: 'css-loader', options: { modules: true, importLoaders: 1, sourceMap: true } },
+              { loader: 'postcss-loader', options: { plugins: [ require('autoprefixer') ], sourceMap: 'inline' }},
+              { loader: 'sass-loader', options: { sourceMap: true }}
+            ]
+            // use: ['css-loader', 'postcss-loader', 'sass-loader']
+          }),
+        },
+      ],
+    }
+  }
+};
